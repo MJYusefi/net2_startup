dc07e17387e59c301dd15cc2879a99f6c4d42d55
Mohammad Javad Yusefi <92368017+MJYusefi@users.noreply.github.com>
2024-11-16 01:49:09 +0330

    Update net2.bat

--- net2.bat
+++ net2.bat
@@ -1,7 +1,7 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 netsh interface ipv4 set dns name="Wi-Fi" DHCP
+@REM [path to net2.py]
 python .\net2.py 
-@REM [path tto net2.py]
 netsh interface ip set dns name="Wi-Fi" source="static" address="178.22.122.100"
 exit
